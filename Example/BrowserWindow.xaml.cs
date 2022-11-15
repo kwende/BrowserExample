411b11f190fd9caed86c7ba49f986cf6f7aa7bab
Ben Rush <brush@ocuvera.com>
2022-11-15 13:14:30 -0600

    poop

---
+++ Example/BrowserWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Windows;
+
+namespace Example
+{
+    /// <summary>
+    /// Interaction logic for BrowserWindow.xaml
+    /// </summary>
+    public partial class BrowserWindow : Window
+    {
+        public event Action<string> OnNavigated;
+
+        public void Navigate(string url)
+        {
+            Browser.Source = new Uri(url);
+        }
+
+        public BrowserWindow()
+        {
+            InitializeComponent();
+
+            Loaded += BrowserWindow_Loaded;
+        }
+
+        private void BrowserWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Browser.NavigationCompleted += Browser_NavigationCompleted; ;
+            Browser.SourceChanged += Browser_SourceChanged;
+        }
+
+        private void Browser_NavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            Browser.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+        }
+
+        private void CoreWebView2_NewWindowRequested(object? sender, CoreWebView2NewWindowRequestedEventArgs e)
+        {
+            e.Handled = true;
+
+            // Josh: if you want pop-ups to navigate the main window instead of just be supressed, uncomment
+            // below: 
+            //Browser.Source = new Uri(e.Uri);
+        }
+
+        private void Browser_SourceChanged(object? sender, CoreWebView2SourceChangedEventArgs e)
+        {
+            Title = Browser.CoreWebView2.DocumentTitle;
+            AddressBar.Text = Browser.Source.ToString();
+
+            OnNavigated?.Invoke(Browser.Source.ToString());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Browser.Source = new Uri(AddressBar.Text);
+        }
+    }
+}
---
+++ Example/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace Example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BrowserWindow _window1 = null, _window2 = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 1. instantiate the two separate windows
+            _window1 = new BrowserWindow();
+            _window2 = new BrowserWindow();
+
+            // 2. show window 1. 
+            _window1.Show();
+            _window2.Show();
+
+            // 3a. for the purposes of this demo, I only want to know when window 1 navigates to a page
+            _window1.OnNavigated += _window1_OnNavigated;
+            // 3b. start navigating 
+            _window1.Navigate("https://darwin.v7labs.com/workview?dataset=285490&image=68393");
+        }
+
+        private void _window1_OnNavigated(string url)
+        {
+            _window2.Navigate(url);
+        }
+    }
+}
